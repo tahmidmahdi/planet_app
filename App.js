11aaea83cce975f3a11e79ec22809b13cead25de
tahmidmahdi <tahmid5550@gmail.com>
2022-05-22 00:02:12 +0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Text from './src/components/text/text';
+import { spacing } from './src/theme/spacing';
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  })
+
+  if (!loaded) {
+    return(
+      <Text>Font is Loading...</Text>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text preset='h1'>Open up App.js to start working on your app!</Text>
+      <Text style={{ marginTop: spacing[3] }}>Hello World!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
